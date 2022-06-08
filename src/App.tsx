23fa901972e19251eeef3b39144df944cddd635f
julior0518 <julior0518@gmail.com>
2022-06-08 10:25:00 -0400

    pre-clearing

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { Button, Typography, styled  } from "@mui/material";
 import { LoadingButton } from '@mui/lab'
 import AlbumIcon from '@mui/icons-material/Album';
 import AddIcon from '@mui/icons-material/Add';
+import {theme} from './theme'
 
 function App() {
 
-  const RediestButton = styled(LoadingButton)({
-    backgroundColor: "firebrick",
+  const RediestButton = styled(LoadingButton)(({theme}) => ({
+    backgroundColor: theme.palette.primary.dark,
     color: "white",
     margin: 5,
-  })
+  }))
 
   return (
     <div className="App">
       <Button variant="text" onClick={()=>{alert('click mf')}}>text</Button>
-      <Button variant="contained" disabled color="secondary">contained</Button>
+      <Button variant="contained"  color="secondary">contained</Button>
       <Button variant="outlined"  endIcon={<AlbumIcon/>}>outlined</Button>
       <LoadingButton
           loading
@@ -33,7 +34,7 @@ function App() {
         Started
       </Button>
       <RediestButton loading> cool </RediestButton>
-      <Button variant="outlined" startIcon={<AddIcon/>} sx={{margin: 5}}>follow</Button>
+      <Button color="secondary" variant="outlined" startIcon={<AddIcon/>} sx={{margin: 5}}>follow</Button>
 
     </div>
   );
---
+++ src/theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+
+    palette:{
+        primary:{
+            main: "#390099",
+            light: "#FF5400 ",
+            dark: "firebrick"
+        },
+        secondary:{
+            main: "#F7D4BC",
+            dark: "#C98BB9"
+        }
+        
+    }
+})
